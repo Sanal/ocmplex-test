4bfc937ec766d28e8cbf2dde19603f9a8a0f32be
Sanal Tserenov <sanaltserenov@outlook.com>
2025-06-23 17:39:24 +0300

    update reviews styles

--- src/modules/reviews/reviews.tsx
+++ src/modules/reviews/reviews.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from "react";
 import { ReviewCard } from "@/components/review-card";
+import styles from "./reviews.module.scss";
 
 async function fetchReviews(): Promise<Review[]> {
   const res = await fetch("http://o-complex.com:1337/reviews", {
@@ -14,9 +15,9 @@ async function ReviewsSection() {
   const reviews = await fetchReviews();
 
   return (
-    <section>
-      <h2>Отзывы</h2>
-      <ul>
+    <section className={styles.container}>
+      <h2 className="visually-hidden">Отзывы</h2>
+      <ul className={styles.reviewsGrid}>
         {reviews.map((review) => (
           <li key={review.id}>
             <ReviewCard content={review.text} />
